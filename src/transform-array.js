9717f912a7247cfdf05f9ac6c78b8c64bec39d33
Andriy Polyakov <polyandit@gmail.com>
2022-10-13 02:36:44 +0300

    iplement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,36 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
+
 function transform(arr) {
   if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
   let resArr = [];
-  for (let i = 0; i < arr.length; ) {
-
-    if (typeof arr[i] == 'number') {
-      resArr.push(arr[i]);
-      i++;
-      continue;
-    }
-    if (arr[i] == '--double-prev' && arr[i - 1]) {
-      resArr.push(arr[i - 1]);
-      i++;
-      continue;
-    }
-    if (arr[i] == '--discard-prev' && resArr.length > 0) {
-      resArr.pop();
-      i++;
-      continue;
-    }
-    if (arr[i] == '--double-next') {
-      resArr.push(arr[i + 1]);
-      i++;
-      continue;
-    }
-    if (arr[i] == '--discard-next' && arr[i + 1]) {
+  arr.forEach(element => {
+    resArr.push(element);
+  });
+  console.log(resArr)
+  for (let i = 0; i < resArr.length;) {
+    console.log(resArr[i]);
+    if (resArr[i] == '--double-prev' && resArr[i - 1]) {
+      resArr.splice(i - 1, 0, resArr[i - 1]);
       i += 2;
       continue;
     }
-    console.log(resArr)
+    if (resArr[i] == '--discard-prev' && resArr[i - 1]) {
+      resArr.splice(i - 1, 1);
+      continue;
+    }
+    if (resArr[i] == '--double-next' && resArr[i + 1]) {
+      resArr.splice(i + 1, 0, resArr[i + 1]);
+      i++;
+      continue;
+    }
+    if (resArr[i] == '--discard-next' && resArr[i + 1]) {
+      resArr.splice(i + 1, 1);
+      i = i + 1;
+      continue;
+    }
+    else i++;
+  }
+  for (let j = 0; j < resArr.length;) {
+    if (resArr[j] == '--double-prev' || resArr[j] == '--discard-prev' || resArr[j] == '--double-next' || resArr[j] == '--discard-next') {
+      resArr.splice(j, 1);
+      continue;
+    }
+    else j++;
   }
   return resArr;
 }
@@ -53,3 +60,7 @@ function transform(arr) {
 module.exports = {
   transform
 };
+
+
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
+// console.log(transform(['--double-prev', 1, 2, 3]));
