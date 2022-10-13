903a336f598e0e716f40e8105a97ea78fccd18aa
Andriy Polyakov <polyandit@gmail.com>
2022-10-13 23:35:42 +0300

    implement extended-repeater task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,29 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value) this.chain.push(`( ${value} )`);
+    else this.chain.push('( )');
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position == 'number' &&
+      Number.isInteger(position) &&
+      this.chain.length >= position > 0)
+      this.chain.splice(position - 1, 1);
+    else throw new Error("You can't remove incorrect link!");
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let simpleChain = this.chain.join("~~");
+    this.chain = [];
+    return simpleChain;
   }
 };
 
