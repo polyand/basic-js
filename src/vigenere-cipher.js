c255377ce9bf62ec83c971f7ea083ecd342e7204
Andriy Polyakov <polyandit@gmail.com>
2022-10-15 00:05:03 +0300

    implement vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    else {
+      message = message.toUpperCase().split(/\B|/);
+      key = key.toUpperCase().split(/\B|/);
+      while (message.length > key.length) key = key.concat(key);
+      let cipher = [];
+      for (let i = 0; i < message.length; i++) {
+        if (!/[A-Z]/.test(message[i])) key.splice(i, 0, message[i])
+        for (let j = 0; j < 26; j++) {
+          if (message[i] === this.alphabet[j]) {
+            message[i] = j;
+            break;
+          }
+        }
+        for (let j = 0; j < 26; j++) {
+          if (key[i] === this.alphabet[j]) {
+            key[i] = j;
+            break;
+          }
+        }
+        if (typeof message[i] === 'number') {
+          message[i] + key[i] < 26 ? cipher.push(this.alphabet[message[i] + key[i]]) : cipher.push(this.alphabet[message[i] + key[i] - 26]);
+        } else {
+          cipher.push(message[i]);
+        }
+      }
+      return this.direct == false ? cipher.reverse().join('') : cipher.join('');
+    }
+  }
+  
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    else {
+      message = message.toUpperCase().split(/\B|/);
+      key = key.toUpperCase().split(/\B|/);
+      while (message.length > key.length) key = key.concat(key);
+      let decipher = [];
+      for (let i = 0; i < message.length; i++) {
+        if (!/[A-Z]/.test(message[i])) key.splice(i, 0, message[i])
+        for (let j = 0; j < 26; j++) {
+          if (message[i] === this.alphabet[j]) {
+            message[i] = j;
+            break;
+          }
+        }
+        for (let j = 0; j < 26; j++) {
+          if (key[i] === this.alphabet[j]) {
+            key[i] = j;
+            break;
+          }
+        }
+        if (typeof message[i] === 'number') {
+          message[i] - key[i] >= 0 ? decipher.push(this.alphabet[message[i] - key[i]]) : decipher.push(this.alphabet[message[i] - key[i] + 26]);
+        } else {
+          decipher.push(message[i]);
+        }
+      }
+      return this.direct == false ? decipher.reverse().join('') : decipher.join('');
+    }
   }
 }
 
